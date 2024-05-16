b1f354e93d0d36f5d0c5d6845d638bc6f0b5ccf3
Dynavy <pl2023379@365.stucom.com>
2024-05-16 23:30:51 +0200

    Feature (ProductsView) : display inventory information finished

--- src/view/ProductsView.java
+++ src/view/ProductsView.java
@@ -1,47 +1,39 @@
 package view;
 
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.Font;
-
-import javax.swing.JButton;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JDialog;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
-
 import main.Shop;
-
+import model.Product;
 import javax.swing.JLabel;
 import javax.swing.JSeparator;
-import java.awt.BorderLayout;
-
 public class ProductsView extends JDialog {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private JTable productTable;
 	private JLabel availableProductsInput;
 	private JSeparator separatorLine;
 	private Shop shop;
 	int option;
-	
 
 	public static void main(String[] args) {
-	
-	}
 
+	}
 
 	public ProductsView(int option, Shop shop) {
 		// Inicialize the same instance and option from ShopView class.
 		this.shop = shop;
 		this.option = option;
-		
-		
+
 		initWindowUI();
 		productsViewUI();
-		
+		tableInformation();
+
 	}
 
 	public void initWindowUI() {
@@ -56,38 +48,58 @@ public class ProductsView extends JDialog {
 		setLocationRelativeTo(null);
 		// We define our background color.
 		setBackground(new Color(237, 237, 233));
+		getContentPane().setLayout(null);
 	}
 
 	public void productsViewUI() {
-		
-		getContentPane().setLayout(null);
+
+
 		Font titleFont = new Font("Poppins", Font.PLAIN, 19);
-		// Product table.
-		productTable = new JTable();
-		productTable.setBounds(61, 57, 226, 289);
-		productTable.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"Name", "PublicPrice", "WholesalerPrice", "Stock"
-			}
-		));
-		getContentPane().add(productTable);
 		
 		
+		// Title text.
 		availableProductsInput = new JLabel("AVAILABLE PRODUCTS");
 		availableProductsInput.setBounds(74, 11, 212, 26);
 		availableProductsInput.setForeground(new Color(0, 108, 34));
 		availableProductsInput.setFont(titleFont);
 		getContentPane().add(availableProductsInput);
-		
+
+		// Green line.
 		separatorLine = new JSeparator();
 		separatorLine.setBounds(68, 34, 212, 11);
 		separatorLine.setForeground(new Color(0, 108, 34));
 		separatorLine.setBackground(new Color(0, 108, 34));
 		getContentPane().add(separatorLine);
 
+	}
+
+	public void tableInformation() {
+
+		// List of Product class with the inventory information.
+		ArrayList<Product> productsInfo = shop.getInventory();
+
+		// Table column information.
+		String[] columnNames = { "Id", "Name", "PublicPrice", "WholesalerPrice ", "Stock" };
+
+		Object[][] data = new Object[productsInfo.size()][5];
+
+		for (int i = 0; i < productsInfo.size(); i++) {
+			
+			Product product = productsInfo.get(i);
+			data[i][0] = product.getId();
+			data[i][1] = product.getName();
+			data[i][2] = product.getPublicPrice();
+			data[i][3] = product.getWholesalerPrice();
+			data[i][4] = product.getStock();
+		}
 		
+		productTable = new JTable(data, columnNames);
+		JScrollPane scrollPane = new JScrollPane(productTable);
+		
+		// Set size of the table.
+		scrollPane.setBounds(10, 50, 310, 300);
+		getContentPane().add(scrollPane);
 	}
 	
+
 }
