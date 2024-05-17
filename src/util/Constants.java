ee24d9574d087b51b8fafb0a96d87f9cdc286550
Dynavy <pl2023379@365.stucom.com>
2024-05-17 11:45:27 +0200

    Added (Constants) : new constants to manage the coordinates of popups.

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -8,5 +8,7 @@ public class Constants {
 	public static final int SHOW_INVENTORY = 5;
 	public static final int DELETE_PRODUCT = 9;
 	public static final int MAX_ATTEMPTS = 3;
+	public static final double DEFAULT_RELATIVE_X = 0.505;
+	public static final double DEFAULT_RELATIVE_Y = 0.348;
 
 }
