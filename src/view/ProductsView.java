e25c2f992871f26be5ebe5d6091ac008560d41cb
Dynavy <pl2023379@365.stucom.com>
2024-05-17 12:08:25 +0200

    Feature (ProductsView) : added backButton to dispose() the JDialog.

--- src/view/ProductsView.java
+++ src/view/ProductsView.java
@@ -2,7 +2,11 @@ package view;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -10,18 +14,19 @@ import main.Shop;
 import model.Product;
 import javax.swing.JLabel;
 import javax.swing.JSeparator;
-public class ProductsView extends JDialog {
+
+public class ProductsView extends JDialog implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
-
 	private JTable productTable;
 	private JLabel availableProductsInput;
 	private JSeparator separatorLine;
+	private JButton backButton;
 	private Shop shop;
 	int option;
-	
+
 	public ProductsView(int option, Shop shop) {
-		
+
 		// Inicialize the same instance and option from ShopView class.
 		this.shop = shop;
 		this.option = option;
@@ -44,9 +49,9 @@ public class ProductsView extends JDialog {
 	}
 
 	public void productsViewUI() {
-		
+
 		Font titleFont = new Font("Poppins", Font.PLAIN, 19);
-		
+
 		// Title text.
 		availableProductsInput = new JLabel("AVAILABLE PRODUCTS");
 		availableProductsInput.setBounds(74, 11, 212, 26);
@@ -60,6 +65,15 @@ public class ProductsView extends JDialog {
 		separatorLine.setForeground(new Color(0, 108, 34));
 		separatorLine.setBackground(new Color(0, 108, 34));
 		getContentPane().add(separatorLine);
+
+		// Back button.
+		backButton = new JButton("BACK");
+		backButton.addActionListener(this);
+		backButton.setBounds(110, 321, 117, 29);
+		getContentPane().add(backButton);
+		
+		// Interaction with the button.
+		backButton.addActionListener(this);
 
 	}
 
@@ -79,10 +93,10 @@ public class ProductsView extends JDialog {
 			data[i][3] = product.getWholesalerPrice();
 			data[i][4] = product.getStock();
 		}
-		
+
 		productTable = new JTable(data, columnNames);
 		JScrollPane scrollPane = new JScrollPane(productTable);
-		
+
 		// Modify width for each column.
 		productTable.getColumnModel().getColumn(0).setPreferredWidth(20); // Id.
 		productTable.getColumnModel().getColumn(1).setPreferredWidth(80); // Name.
@@ -90,9 +104,15 @@ public class ProductsView extends JDialog {
 		productTable.getColumnModel().getColumn(3).setPreferredWidth(110); // WholeSalerPrice.
 		productTable.getColumnModel().getColumn(4).setPreferredWidth(40); // Stock
 		// Set size of the table.
-		scrollPane.setBounds(10, 50, 310, 300);
+		scrollPane.setBounds(10, 50, 310, 264);
 		getContentPane().add(scrollPane);
 	}
-	
 
+	@Override
+	public void actionPerformed(ActionEvent inputButton) {
+
+		if (inputButton.getSource() == backButton) {
+			dispose();
+		}
+	}
 }
