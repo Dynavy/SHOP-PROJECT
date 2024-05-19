646e12ce85b1fdcf6ce2796a6c73a2952535cb0a
Dynavy <pl2023379@365.stucom.com>
2024-05-19 15:03:32 +0200

    ReFactor (Constants) : removed constant 'CHECK_MONEY'
    - It was unnecessary.

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -2,13 +2,14 @@ package util;
 
 public class Constants {
 
-	public static final int CHECK_MONEY = 1;
 	public static final int ADD_PRODUCTS = 2;
 	public static final int ADD_STOCK = 3;
 	public static final int SHOW_INVENTORY = 5;
+	public static final int MAKE_SALE = 6;
 	public static final int DELETE_PRODUCT = 9;
 	public static final int MAX_ATTEMPTS = 3;
 	public static final double DEFAULT_RELATIVE_X = 0.505;
 	public static final double DEFAULT_RELATIVE_Y = 0.348;
+
 
 }
