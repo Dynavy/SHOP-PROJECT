4498b8a17092b1f821ae459bbedf5d430c724876
Dylan Navarro Vinyarta <159908255+Dynavy@users.noreply.github.com>
2024-10-06 13:05:45 +0200

    ReFactor(Dao): Added two (2) new methods.

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -1,12 +1,17 @@
 package dao;
 
 import java.sql.SQLException;
-
+import java.util.ArrayList;
 import model.Employee;
+import model.Product;
 
 public interface Dao {
+	
 
 	void connect() throws SQLException;
 	void disconnect() throws SQLException;
-	Employee getEmployee(int user, String pw); 
+	Employee getEmployee(int user, String pw);
+	// New methods to manage the inventory.
+	ArrayList<Product> getInventory();
+	boolean writeInventory(ArrayList<Product> product);
 }
