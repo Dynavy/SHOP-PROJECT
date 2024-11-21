69196a366503d617b8ac87930cd7356731f0b847
Dylan Navarro Vinyarta <159908255+Dynavy@users.noreply.github.com>
2024-11-21 19:47:22 +0100

    Fix(JaxbUnMarshaller): don't manage anymore the totaProductss (ID).

--- src/dao/jaxb/JaxbUnMarshaller.java
+++ src/dao/jaxb/JaxbUnMarshaller.java
@@ -15,12 +15,10 @@ public class JaxbUnMarshaller {
 	public ProductList init() {
 		ProductList products = null;
 		try {
-			
+
 			// Create a JAXB context (instance) for marshalling and unmarshalling ProductList objects.
 			JAXBContext context = JAXBContext.newInstance(ProductList.class);
 			Unmarshaller unmarshaller = context.createUnmarshaller();
-			// Make sure that the ID of the products start at 1.
-			Product.setTotalProducts(1);
 			// Store products on our products List.
 			products = (ProductList) unmarshaller.unmarshal(new File("./jaxb/inputInventory.xml"));
 
