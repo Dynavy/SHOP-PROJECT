4c347c09d363e65e03ede0c65d6f1ca04c96b966
Dynavy <pl2023379@365.stucom.com>
2024-11-25 20:12:07 +0100

    Chore(Product): deleted useless import.

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,7 +1,6 @@
 package model;
 
 import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
