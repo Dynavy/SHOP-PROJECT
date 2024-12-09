f246f8550d183aa7e6b0f406fd981b508c6560f7
Dylan Navarro Vinyarta <159908255+Dynavy@users.noreply.github.com>
2024-12-09 13:39:18 +0100

    Feat(DaoImplJDBC): implemented CRUD methods logic.
    - getInventory() method implemented.
    
    - addProduct() method implemented.
    
    - updateProduct() method implemented.
    
    - deleteProduct() method implemented.

--- src/dao/DaoImplJDBC.java
+++ src/dao/DaoImplJDBC.java
@@ -1,11 +1,13 @@
 package dao;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+
 import model.Employee;
 import model.Product;
 
@@ -14,7 +16,7 @@ public class DaoImplJDBC implements Dao {
 	private Connection connection;
 
 	public DaoImplJDBC() {
-
+		connect();
 	}
 
 	@Override
@@ -63,7 +65,8 @@ public class DaoImplJDBC implements Dao {
 			try (ResultSet rs = ps.executeQuery()) {
 				// If we find any data, we create an Employee object.
 				if (rs.next()) {
-					// Creates an Employee instance to temporarily store the user and password data retrieved from the database.
+					// Creates an Employee instance to temporarily store the user and password data
+					// retrieved from the database.
 					employee = new Employee(user, pw);
 				}
 			}
@@ -75,17 +78,88 @@ public class DaoImplJDBC implements Dao {
 		// We return the employee object.
 		return employee;
 	}
+	
+	@Override
+	public ArrayList<Product> getInventory() {
+	
+		ArrayList<Product> productsList = new ArrayList<>();
+		// Prepare SQL query.
+		String query = "SELECT * FROM Inventory";
 
+		try (PreparedStatement ps = connection.prepareStatement(query);
+			 ResultSet rs = ps.executeQuery()) {
+			        // Iterate through the result set.
+			        while (rs.next()) {
+
+			            Product products = new Product(
+			                      rs.getString("Name"), 
+			                      rs.getBigDecimal("wholesalerPrice").doubleValue(),
+			                      rs.getBoolean("Available"),      
+			                      rs.getInt("Stock") 
+			            );
+			            productsList.add(products);
+			}
+			        
+		} catch (SQLException SqlError) {
+
+			System.out.println("Error connecting with the database" + SqlError.getMessage());
+			SqlError.printStackTrace();
+		}
+		// We return the Product object.
+		return productsList;
+	}
+
+	@Override
+	public void addProduct(Product product) {
+	    String query = "INSERT INTO Inventory (Name, wholesalerPrice, Available, Stock) VALUES (?, ?, ?, ?)";
+
+	    try (PreparedStatement ps = connection.prepareStatement(query)) {
+	        ps.setString(1, product.getName());
+	        ps.setBigDecimal(2, BigDecimal.valueOf(product.getWholesalerPrice().getValue()));
+	        ps.setBoolean(3, product.isAvailable());
+	        ps.setInt(4, product.getStock());
+
+	        ps.executeUpdate();
+	    } catch (SQLException SqlError) {
+	        System.out.println("Error adding product to the database: " + SqlError.getMessage());
+	        SqlError.printStackTrace();
+	    }
+	}
+	
+	@Override
+	public void updateProduct(Product product) {
+	    String query = "UPDATE Inventory SET Name = ?, wholesalerPrice = ?, Available = ?, Stock = ? WHERE Name = ?";
+
+	    try (PreparedStatement ps = connection.prepareStatement(query)) {
+	        ps.setString(1, product.getName());
+	        ps.setBigDecimal(2, BigDecimal.valueOf(product.getWholesalerPrice().getValue()));
+	        ps.setBoolean(3, product.isAvailable());
+	        ps.setInt(4, product.getStock());
+
+	        ps.executeUpdate();
+	    } catch (SQLException SqlError) {
+	        System.out.println("Error updating product in the database: " + SqlError.getMessage());
+	        SqlError.printStackTrace();
+	    }
+	}
+
+	@Override
+	public void deleteProduct(Product product) {
+	    String query = "DELETE FROM Inventory WHERE Name = ?";
+
+	    try (PreparedStatement ps = connection.prepareStatement(query)) {
+	        ps.setString(1, product.getName()); 
+
+	        ps.executeUpdate();
+	    } catch (SQLException SqlError) {
+	        System.out.println("Error deleting product from the database: " + SqlError.getMessage());
+	        SqlError.printStackTrace();
+	    }
+	}
+	
 	@Override
 	public boolean writeInventory(ArrayList<Product> product) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
-	@Override
-	public ArrayList<Product> getInventory() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
