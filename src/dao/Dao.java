6bef9d44fb738e997a73c0af849f98a31eec3483
Dynavy <dynavy@hotmail.com>
2025-02-06 17:00:04 +0100

    CHORE(dao): added blank space between interface methods.

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -7,12 +7,19 @@ import model.Product;
 public interface Dao {
 
 	void connect();
+
 	void disconnect();
+
 	Employee getEmployee(int user, String pw);
+
 	// New methods to manage the inventory.
 	ArrayList<Product> getInventory();
+
 	boolean writeInventory(ArrayList<Product> product);
+
 	void addProduct(Product product);
+
 	void updateProduct(Product product);
+
 	void deleteProduct(Product product);
 }
