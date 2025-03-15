43a10f89bc1481b4cd684618b1af8c0034c9bb08
Dynavy <dynavy@hotmail.com>
2025-03-15 09:54:13 +0100

    REFACTOR(Employee): changed polymorphism to aim DaoImplMongoDB.

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,15 +1,15 @@
 package model;
 
 import dao.Dao;
-import dao.DaoImplJDBC;
+import dao.DaoImplMongoDB;
 import main.Logable;
 
 public class Employee extends Person implements Logable {
 
 	// Polymorphism enables accessing DaoImpl functionalities through the dao object.
-	public Dao dao = new DaoImplJDBC();
+	public Dao dao = new DaoImplMongoDB();
 
-	public Employee(int employee_id, String pw) {
+	public Employee(String username, String pw) {
 		super(name);
 
 	}
@@ -18,14 +18,11 @@ public class Employee extends Person implements Logable {
 
 	}
 
-	public boolean login(int user, String password) {
-		
-		// We made the connection to the database.
-		dao.connect();
+	@Override
+	public boolean login(String user, String password)  {
+	
 		// If we have matches, we store it on the credentials instance.
 		Employee credentials = dao.getEmployee(user, password);
-		// We close the conection to the database after checking the credentials.
-		dao.disconnect();
 		// If we have no matches, we return null.
 		return credentials != null;
 	}
