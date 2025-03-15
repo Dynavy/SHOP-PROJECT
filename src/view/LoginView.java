dead126ff6df1848a640e306229fd20892b869e0
Dynavy <dynavy@hotmail.com>
2025-03-15 09:57:17 +0100

    REFACTOR(LoginView): Changed username variable from int to String.

--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -220,59 +220,53 @@ public class LoginView extends JFrame implements ActionListener, KeyListener {
 	}
 
 	public void validateCredentials() {
-		// Every user introduces wrong credentials, we add 1 to the class variable.
-		countError++;
-		String stringEmployeeID = employeeUser.getText();
-		// We have to assign the password chars to a new array because is a
-		// JpasswordField.
-		char[] passwordChars = employeePass.getPassword();
-		// We transform the chars into a String.
-		String userPassword = new String(passwordChars);
-		// Need this variable to parse the stringEmployeeID.
-		int userID = 0;
+	    // Every user introduces wrong credentials, we add 1 to the class variable.
+	    countError++;
+	    String EmployeeUsername = employeeUser.getText();
+	    // We have to assign the password chars to a new array because it's a JPasswordField.
+	    char[] passwordChars = employeePass.getPassword();
+	    // We transform the chars into a String.
+	    String userPassword = new String(passwordChars);
+	    String userID = EmployeeUsername;
 
-		try {
+	    try {
+	        // The first thing that is going to check is if credentials are invalid for the third consecutive time.
+	        if (countError >= Constants.MAX_ATTEMPTS && !isLogged) {
+	            throw new LimitLoginException();
+	        }
 
-			// The first thing that is going check is if credentials are invalid for the
-			// third consecutive time .
-			if (countError >= Constants.MAX_ATTEMPTS && !isLogged) {
-				throw new LimitLoginException();
-			}
-			// Since we need an int, we parse the variable so it can match as an argument.
-			userID = Integer.parseInt(stringEmployeeID);
+	        isLogged = employee.login(userID, userPassword);
 
-			// It will return true/false to our boolean IsLogged
-			isLogged = employee.login(userID, userPassword);
+	        // Wrong credentials --> returns false.
+	        if (!isLogged) {
+	            // Error message display when incorrect credentials.
+	            JOptionPane.showMessageDialog(LoginView.this,
+	                    "Invalid credentials, try again " + countError + "/" + Constants.MAX_ATTEMPTS + ".",
+	                    "WARNING! VALIDATION WENT WRONG", JOptionPane.ERROR_MESSAGE);
+	        } else {
+	            // Good credentials --> returns true.
+	            dispose();
+	            ShopView shopFrame = new ShopView();
+	            // After doing the dispose on the loginView, we set visible the ShopView.
+	            shopFrame.setVisible(true);
+	            shopFrame.requestFocus();
+	        }
 
-			// Wrong credentials --> returns false.
-			if (!isLogged) {
-				// Error message display when incorrect credentials.
-				JOptionPane.showMessageDialog(LoginView.this,
-						"Invalid credentials, try again " + countError + "/" + Constants.MAX_ATTEMPTS + ".",
-						"WARNING! VALIDATION WENT WRONG", JOptionPane.ERROR_MESSAGE);
-			} else {
-				// Good credentials --> returns true.
-				dispose();
-				ShopView shopFrame = new ShopView();
-				// After doing the dispose on the loginView, we set visible the ShopView.
-				shopFrame.setVisible(true);
-				shopFrame.requestFocus();
-			}
+	    // Only executes when the count error is == 3.
+	    } catch (LimitLoginException maxAttempts) {
+	        JOptionPane.showMessageDialog(LoginView.this, maxAttempts.getMessage(), "LEAVING THE PROGRAM.",
+	                JOptionPane.ERROR_MESSAGE);
+	        dispose();
 
-			// Only executes when the count error is == 3.
-		} catch (LimitLoginException maxAttempts) {
-			JOptionPane.showMessageDialog(LoginView.this, maxAttempts.getMessage(), "LEAVING THE PROGRAM.",
-					JOptionPane.ERROR_MESSAGE);
-			dispose();
-
-			// If it can't parse (its not an int), the catch is going to execute.
-		} catch (NumberFormatException invalidInput) {
-			// User notification about only introducing a natural number.
-			JOptionPane.showMessageDialog(LoginView.this,
-					"Employee ID must be a natural number, try again " + countError + "/3.", "ERROR MESSAGE DISPLAY.",
-					JOptionPane.ERROR_MESSAGE);
-		}
+	    // If it can't parse (its not an int), the catch is going to execute.
+	    } catch (NumberFormatException invalidInput) {
+	        // User notification about only introducing a natural number.
+	        JOptionPane.showMessageDialog(LoginView.this,
+	                "Employee ID must be a natural number, try again " + countError + "/3.", "ERROR MESSAGE DISPLAY.",
+	                JOptionPane.ERROR_MESSAGE);
+	    }
 	}
+
 
 	// We are invoking this everytime user interacts with loginButton.
 	public void resetText() {
